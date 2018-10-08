e8de08bea97d07d77c0ea8ad4ec4d0d11907c59c
Summer Torres <summermt.hiraki@gmail.com>
2018-10-08 01:33:43 -0700

    protoype final revisions js added

---
+++ js/index.js
@@ -0,0 +1,123 @@
+// Select Line Buttons - Row
+var $;
+
+$('.rowA').click(function () {
+  $(this).toggleClass('active');
+});
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowA").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowA").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+$('.rowB').click(function () {
+  $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowB").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowB").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+$('.rowC').click(function () {
+  $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowC").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowC").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+$('.rowD').click(function () {
+  $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowD").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowD").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+$('.rowE').click(function () {
+  $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowE").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowE").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+$('.rowF').click(function () {
+  $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowF").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowF").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+$('.rowG').click(function () {
+  $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".rowG").on('click touchstart', function () {
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".rowG").on('animationend', function () {
+  $(this).toggleClass('is_animating');
+});
+
+
+// Favorite Button - Star
+$('.favstar').click(function() {
+	$(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".favstar").on('click touchstart', function(){
+  $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".favstar").on('animationend', function(){
+  $(this).toggleClass('is_animating');
+});
+
+
