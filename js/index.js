53b8f14af1401e58ce93a9d55cdad5282bc2caaa
Summer Torres <summermt.hiraki@gmail.com>
2018-10-17 19:10:47 -0700

    Bloc requested revisions A

--- js/index.js
+++ js/index.js
@@ -1,106 +1,16 @@
 // Select Bus Line by Row
 var $;
 
-$('.rowA').click(function () {
+$('.row').click(function () {
   $(this).toggleClass('active');
 });
 /* when a user clicks, toggle the 'is-animating' class */
-$(".rowA").on('click touchstart', function () {
+$(".row").on('click touchstart', function () {
   $(this).toggleClass('is_animating');
 });
 
 /*when the animation is over, remove the class*/
-$(".rowA").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
-
-
-$('.rowB').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".rowB").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".rowB").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
-
-
-$('.rowC').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".rowC").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".rowC").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
-
-
-$('.rowD').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".rowD").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".rowD").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
-
-
-$('.rowE').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".rowE").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".rowE").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
-
-
-$('.rowF').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".rowF").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".rowF").on('animationend', function () {
-  $(this).toggleClass('is_animating');
-});
-
-
-$('.rowG').click(function () {
-  $(this).toggleClass('active');
-});
-
-/* when a user clicks, toggle the 'is-animating' class */
-$(".rowG").on('click touchstart', function () {
-  $(this).toggleClass('is_animating');
-});
-
-/*when the animation is over, remove the class*/
-$(".rowG").on('animationend', function () {
+$(".row").on('animationend', function () {
   $(this).toggleClass('is_animating');
 });
 
@@ -119,5 +29,3 @@ $(".favstar").on('click touchstart', function(){
 $(".favstar").on('animationend', function(){
   $(this).toggleClass('is_animating');
 });
-
-
